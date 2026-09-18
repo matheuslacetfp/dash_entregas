0f19c8717ba672071616213daa7c3e6cf792c0eb
Matheus Lacet <matheus.lacet@fernandapessoa.com.br>
2026-09-18 14:48:05 -0300

    fix: correct supabase project url

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_CONFIG = {
   // Replace these values with the Supabase Project URL and anon public key.
-  url: "https://wkxwbugntpmxmbteuy.supabase.co",
+  url: "https://wkxwbugntpmxmbbteuyy.supabase.co",
   anonKey: "sb_publishable__0U-F7p4KxTlWCfjD-jYWA_8bSkFYcG",
 };
